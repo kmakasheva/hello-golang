60072d47debc43a38b8c354aa74d8812264aeac2
Kamila Makasheva <kmakasheva1@gmail.com>
2025-01-16 17:59:42 +0500

    fix ubuntu-2

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	want := "Hello Go"
+	want := "Hello Go!"
 
 	if got := Hello(); got != want {
 		t.Errorf("Hello() = %q, want %q", got, want)
